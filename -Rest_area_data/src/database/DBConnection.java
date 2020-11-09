421867fb0fe9462641ca8b2777147922ebd7a0fa
kimmingyu <kimmin36944@gmail.com>
2020-11-09 12:25:17 +0900

    first commit

---
+++ -Rest_area_data/src/database/DBConnection.java
@@ -0,0 +1,30 @@
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection 
+{
+	private final static String JDCB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private final static String DB_URL = "jdbc:mysql://14.38.252.76/account?characterEncoding=UTF-8&serverTimezone=UTC";//mysql 주소
+	private final static String USER_NAME = "aoop";//아이디
+	private final static String PASSWORD = "0978";//비번
+	private static Connection con = null;
+	private DBConnection() {}//접근 x
+	public static Connection getInstance()
+	{
+		if(con==null)
+		{
+			try {
+				Class.forName(JDCB_DRIVER);
+				con = DriverManager.getConnection(DB_URL,USER_NAME,PASSWORD);
+				System.out.println("///////DB연결됨//////");
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				System.out.println("연결오류");
+			}
+		}
+		return con;
+	}
+	
+}
